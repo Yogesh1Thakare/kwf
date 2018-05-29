81e6f88d17b199f1df4488c6653a2aee12a869c8
ALBERT EINSTINE <ALBERT EINSTINE@ALBERTEINSTINE>
2018-05-29 23:44:11 +0530

    kwf

---
+++ src/practice1/Test1.java
@@ -0,0 +1,20 @@
+package practice1;
+
+public class Test1 
+{
+
+	public static void main(String[] args) 
+	{
+		int age=2;  
+	    if(age>18)
+	{  
+	        System.out.print("Age is greater than 18");
+
+	}
+	    else
+	    {
+	    	System.out.println("age is less than 18");
+	    }
+
+}
+}
